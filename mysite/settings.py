3e502e0513f1a00ffb9972c61f596b4b0f381d15
micaelagimenez <micaelagimenez@live.com.ar>
2021-05-15 11:50:32 -0300

    debugging added

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,3 +131,37 @@ STATIC_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+ # Debugging in heroku live
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
